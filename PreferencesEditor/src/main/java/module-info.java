4321f2dc177cfbc2aaaca7b3a76998d8ad8cb46d
Alan T. Coté <alantcote@gmail.com>
2022-01-22 14:22:17 -0500

    Set up PreferencesEditor to use WindowPrefs from CLUtilities.

--- PreferencesEditor/src/main/java/module-info.java
+++ PreferencesEditor/src/main/java/module-info.java
@@ -5,11 +5,12 @@
 module preferenceseditor {
 	exports net.sf.cotelab.preferenceseditor;
 	
+	requires clutilities;
 	requires java.prefs;
 	requires javafx.base;
-	requires transitive javafx.graphics;
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires transitive javafx.graphics;
 	requires javafx.web;
 	
 	opens net.sf.cotelab.preferenceseditor to javafx.fxml;
--- PreferencesEditor/src/main/java/net/sf/cotelab/preferenceseditor/PreferencesEditor.java
+++ PreferencesEditor/src/main/java/net/sf/cotelab/preferenceseditor/PreferencesEditor.java
@@ -1,32 +1,54 @@
 package net.sf.cotelab.preferenceseditor;
-	
+
+import java.util.prefs.BackingStoreException;
+
+import cotelab.util.javafx.windowprefs.WindowPrefs;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
+import javafx.stage.WindowEvent;
 
 public class PreferencesEditor extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	protected WindowPrefs windowPrefs = null;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("PreferencesEditor.fxml"));
+			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("PreferencesEditor.fxml"));
 //			Scene scene = new Scene(root,1000,900);
 			Scene scene = new Scene(root);
-			
+
 			root.setUserData(getHostServices()); // covert communication to controller
-			
+
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Preferences Editor");
+
+			primaryStage.setOnShown(new EventHandler<WindowEvent>() {
+
+				@Override
+				public void handle(WindowEvent event) {
+					try {
+						windowPrefs = new WindowPrefs(PreferencesEditor.class, primaryStage);
+					} catch (BackingStoreException e) {
+						System.err.println("FileTreeViewDemo.start(): caught: " + e.getMessage());
+						e.printStackTrace();
+						System.err.println("FileTreeViewDemo.start(): continuing . . .");
+					}
+				}
+
+			});
+
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
 	}
 }
